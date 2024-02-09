e7f54747f462edc5ab3c484e137feff42fc207d8
Guilherme Coelho <93000522+oKrolik@users.noreply.github.com>
2024-02-09 13:04:00 +0000

    Minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -13,65 +13,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(
-                color: Colors.red,
-                width: 100,
-                height: 100,
-              ),
-              Container(
-                color: Colors.blue,
-                width: 50,
-                height: 50,
-              ),
-            ],
-          ),
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(
-                color: Colors.blue,
-                width: 100,
-                height: 100,
-              ),
-              Container(
-                color: Colors.red,
-                width: 50,
-                height: 50,
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                color: Colors.cyan,
-                height: 50,
-                width: 50,
-              ),
-              Container(
-                color: Colors.pinkAccent,
-                height: 50,
-                width: 50,
-              ),
-              Container(
-                color: Colors.purple,
-                height: 50,
-                width: 50,
-              )
-            ],
-          ),
-        ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter: meus primeiros passos'),
+        ),
+        body: Container(color: Colors.black,),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('Você pressionou o botão.');
+          },
+        ),
       ),
     );
   }
